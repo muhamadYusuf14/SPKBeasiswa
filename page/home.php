f12143c1e602940b9081c93869b4e6c081a9cef2
muhamadYusuf14 <149951792+muhamadYusuf14@users.noreply.github.com>
2024-07-12 22:12:54 +0700

    Add files via upload

---
+++ page/home.php
@@ -0,0 +1,10 @@
+<body bgcolor="Pink">
+<h1><p align="center">
+<font color="maroon">
+   	Selamat Datang di Sistem Pendukung Keputusan <br>
+	Penentuan Penerima Beasiswa di Politeknik Baja Tegal <br>
+	Menggunakan Metode Simple Additive Weighting (SAW)
+</h1> 
+</b>
+</p>
+</font>
